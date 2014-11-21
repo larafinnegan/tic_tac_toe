94ad86c8ba0867c52a4263f08196595ba4910dbe
Lara Finnegan <lcfinnegan@gmail.com>
2014-11-20 21:27:20 -0600

    basic working program.  It's pretty ugly

---
+++ player.rb
@@ -0,0 +1,8 @@
+class Player
+attr_accessor :name, :preference
+
+	def initialize(name, preference)
+		@name = name
+		@preference = preference
+	end
+end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,98 +1,124 @@
-
-
-    puts "Instructions"
-    puts "Please enter the name of player 1:"
-    name1 = gets.chomp
-
-    puts "Please enter the name of player 2:"
-    name2 = gets.chomp
-
-if rand(2) == 0
-    preference1 = " O "
-    preference2 = " X "
-    order1 = 1
-    order2 = 2
-else
-    preference2 = " O "
-    preference1 = " X "
-    order1 = 2
-    order2 = 1
-end
-
-puts "To be fair, the computer has randomly decided which player 
-will play X and which will play O."
-
-possible_inputs = [7,8,9,4,5,6,1,2,3]
-board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-
-def display_game(board)
-puts board[0..2].join("|")
-puts "-----------"
-puts board[3..5].join("|")
-puts "-----------"
-puts board[6..8].join("|")
-end
-
-
-display_game(board)
-
 class Player
-attr_accessor :name, :preference, :order
+attr_accessor :name, :preference
 
-def initialize(name, preference, order)
-    @name = name
-    @preference = preference
-    @order = order
+	def initialize(name, preference)
+		@name = name
+		@preference = preference
 	end
 end
 
-player1 = Player.new(name1, preference1, order1)
-player2 = Player.new(name2, preference2, order2)
+class Board
 
-puts player1.name + ":" + player1.preference
-puts player2.name + ":" + player2.preference
-
-puts "To be fair, the computer has randomly decided which player 
-will go first."
-
-if order2 < order1
-current_player = player1
-else current_player = player2
-end
-
-puts "#{current_player.name}, please take your turn:"
-turn = gets.chomp
-turn = turn.to_i
-while possible_inputs.include?(turn) == false
-puts "Invalid input. Please use keys 1 through 9"
-turn = gets.chomp
-turn = turn.to_i
-    while board[turn-1] != "   "
-    puts "Invalid choice.  That turn has been taken."
-    turn = gets.chomp
-    turn = turn.to_i
+	@@possible_inputs = [7,8,9,4,5,6,1,2,3]
+    
+    def initialize
+        @area = Array.new(9)    
     end
-end
-board[possible_inputs.index(turn)] = current_player.preference
-display_game(board)
 
-def win?(board, pref)
-if board[0]+board[1]+board[2] == pref * 3 ||
-    board[3]+board[4]+board[5] == pref * 3 ||
-    board[6]+board[7]+board[8] == pref * 3 ||
-    board[0]+board[3]+board[6] == pref * 3 ||
-    board[1]+board[4]+board[7] == pref * 3 ||
-    board[2]+board[5]+board[8] == pref * 3 ||
-    board[0]+board[4]+board[8] == pref * 3 ||
-    board[2]+board[4]+board[6] == pref * 3
-    return true
-    end
+	def display_game
+		puts @area[0..2].join("  |  ")
+		puts "-----------"
+		puts @area[3..5].join("  |  ")
+		puts "-----------"
+		puts @area[6..8].join("  |  ")
+	end
+
+	def invalid_input(input)
+		true if !@@possible_inputs.include?(input)
+	end
+
+	def already_taken(input)
+		true if @area[@@possible_inputs.index(input)] != nil
+	end
+
+	def take_turn(turn, value)
+		@area[@@possible_inputs.index(turn)] = value
+	end
+	
+	def win?
+		win = ["XXX","OOO"]
+		if win.include?(@area[0..2].join("")) ||
+			win.include?(@area[3..5].join("")) ||
+			win.include?(@area[6..8].join("")) ||
+			win.include?([@area[0], @area[3], @area[6]]) ||
+			win.include?([@area[1], @area[4], @area[7]]) ||
+			win.include?([@area[2], @area[5], @area[8]]) ||
+			win.include?([@area[0], @area[4], @area[8]]) ||
+			win.include?([@area[2], @area[4], @area[6]])
+			return true
+		end
+	end
+
+	def tie?
+		@area.all? {|x| x != nil}
+	end
 end
 
-def tie?(board)
-board.all? {|x| x != "   "}
+class Game
+    def initialize
+		@board = Board.new
+		
+		puts "\n\nINSTRUCTIONS"
+		puts "Use the 1-9 keys to make your turn.\n\n"
+		puts "         7 | 8 | 9 "
+		puts "        -----------"
+		puts "         4 | 5 | 6 "
+		puts "        -----------"
+		puts "         1 | 2 | 3 "
+		puts "\n\nPlease enter the name of player 1:"
+		name1 = gets.chomp
+		puts "\n\nPlease enter the name of player 2:"
+		name2 = gets.chomp
+		x_or_o = ["X","O"]
+		x_or_o.shuffle
+		
+		@players = [Player.new(name1, x_or_o[0]), Player.new(name2, x_or_o[1])]
+		@players.shuffle
+        
+		puts "\n\n#{@players[0].name} has been randomly assigned to #{@players[0].preference}."
+		puts "Therefore, #{@players[1].name} has been assigned to #{@players[1].preference}."
+		puts "The computer has randomly decided who will go first.\n\n"
+	end
+
+	def switch_players
+    	@players[1], @players[0] = @players[0], @players[1]
+		@current_player = @players[0]
+	end
+
+
+	def new_turn
+		switch_players
+		puts "\n#{@current_player.name}, please take your turn:"
+		turn = gets.chomp.to_i
+		while @board.invalid_input(turn) || @board.already_taken(turn)
+			puts "Invalid input.  Please use keys 1-9 to choose an empty position."
+			@board.display_game
+			turn = gets.chomp.to_i
+		end
+		@board.take_turn(turn, @current_player.preference)
+	end
+
+	def game_over?
+		if @board.tie? || @board.win?
+			true
+		else
+			false
+		end
+	end
+
+	def play
+		while game_over? == false
+			new_turn
+			@board.display_game
+		end
+		puts "Game over!"
+		if @board.win?
+			puts "Congrats, #{@current_player.name}!!" 
+		else 
+			puts "Tie!"
+		end
+	end
 end
 
-def game_over?
-true if tie? || win?
-end
+game = Game.new
+game.play
