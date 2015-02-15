e0e815ebe0036dcccca48335647df277e91b9ded
Lara Finnegan <lcfinnegan@gmail.com>
2015-02-15 14:10:22 -0600

    fixed tabification issues

--- board.rb
+++ board.rb
@@ -1,42 +1,42 @@
 class Board
-attr_accessor :board
+  attr_accessor :board
 
-    def initialize(board = Array.new(9, "-"))
-        @board = board
-    end
+  def initialize(board = Array.new(9, "-"))
+    @board = board
+  end
 
-	def display_game
-		puts board[0..2].join("|")
-		puts board[3..5].join("|")
-		puts board[6..8].join("|")
-	end
+  def display_game
+    puts board[0..2].join("|")
+    puts board[3..5].join("|")
+    puts board[6..8].join("|")
+  end
 
-	def move_valid?(input)
-		board[input - 1] == "-"
-	end
+  def move_valid?(input)
+    board[input - 1] == "-"
+  end
 
-	def take_turn(turn, value)
-		board[turn - 1] = value
-	end
-	
-	def win?(input)
-		board[0..2].join == (input * 3) ||
-		board[3..5].join == (input * 3) ||
-		board[6..8].join == (input * 3) ||
-		[board[0], board[3], board[6]].join == (input * 3) ||
-		[board[1], board[4], board[7]].join == (input * 3) ||
-		[board[2], board[5], board[8]].join == (input * 3) ||
-		[board[0], board[4], board[8]].join == (input * 3) ||
-		[board[2], board[4], board[6]].join == (input * 3)
-	end
+  def take_turn(turn, value)
+    board[turn - 1] = value
+  end
 
-	def tie?
-		board.none? {|x| x == "-"}
-	end	
+  def win?(input)
+    board[0..2].join == (input * 3) ||
+    board[3..5].join == (input * 3) ||
+    board[6..8].join == (input * 3) ||
+    [board[0], board[3], board[6]].join == (input * 3) ||
+    [board[1], board[4], board[7]].join == (input * 3) ||
+    [board[2], board[5], board[8]].join == (input * 3) ||
+    [board[0], board[4], board[8]].join == (input * 3) ||
+    [board[2], board[4], board[6]].join == (input * 3)
+  end
+
+  def tie?
+    board.none? {|x| x == "-"}
+  end	
 
   def game_over?
     board.tie? || board.win?(@players[0].preference)
   end
 
-  
+
 end
